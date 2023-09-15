dd1efbb14d6b43f879a74ecb379a77ca113a64fb
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-15 20:09:02 +0200

    added lodash

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
-import './style.css'
+import './style.css';
+import { sortBy } from 'lodash-es';
+
+const array = [
+  { name: 'John', age: 30 },
+  { name: 'John', age: 15 },
+  { name: 'Jane', age: 25 }
+];
+const sortedArray = sortBy(array, ['name', 'age']);
+console.log(array);
+console.log(sortedArray);
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
   <div>
